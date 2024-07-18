7aebdc158fc176a4efb55188391a1ec53e05a025
victormanuelLR <vleal3423@gmail.com>
2024-07-18 15:49:10 -0300

    Atualizadas as mensagens do sistema

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { turboLogger } from "./turboLogger.js";
 
 console.log("Estou usabdo o github");
-turboLogger("estou aprendendo o fluxo do git", 'red')
+turboLogger("estou aprendendo o fluxo do git", 'red');
+turboLogger("Eu consegui criar uma nova mensagem sem afetar a versão principal", 'darkgreen')
